db8d923ab51520187d6cf3b1a564791d3e3a4b4c
PraveenRanasinghe <praveenranasinghe43@gmail.com>
2021-08-03 02:16:20 +0530

    Working on Admin Dashboard

--- app/src/main/java/com/example/my_timetable/add_student.java
+++ app/src/main/java/com/example/my_timetable/add_student.java
@@ -1,14 +1,52 @@
 package com.example.my_timetable;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class add_student extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+
+        drawerLayout=findViewById(R.id.drawerLayout);
     }
+
+    public void clickMenu(View view){
+        openDrawer(drawerLayout);
+    }
+
+    private static void openDrawer(DrawerLayout drawerLayout) {
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+//    public void clickLogo(View view){
+//        closeDrawer(drawerLayout);
+//    }
+//
+//    private static void closeDrawer(DrawerLayout drawerLayout) {
+//        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+//            drawerLayout.closeDrawer(GravityCompat.START);
+//        }
+//    }
+
+    public void ClickHome(View view){
+         redirectToActivity(add_student.this,Admin.class);
+    }
+
+    public static void redirectToActivity(Activity activity, Class aClass){
+        Intent intent = new Intent(activity,aClass);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        activity.startActivity(intent);
+    }
+
 }
