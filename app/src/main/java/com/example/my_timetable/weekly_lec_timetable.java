1b49c76a71a37c7e133993723b1c6bd2eabfabc1
PraveenRanasinghe <praveenranasinghe43@gmail.com>
2021-08-13 22:09:44 +0530

    Working on Recycler View

---
+++ app/src/main/java/com/example/my_timetable/weekly_lec_timetable.java
@@ -0,0 +1,14 @@
+package com.example.my_timetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class weekly_lec_timetable extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weekly_lec_timetable);
+    }
+}
